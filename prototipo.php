7d1024f1e22588d148d754a887ac9dc5be0cfddf
Dmaus5 <90470689+Dmaus5@users.noreply.github.com>
2021-09-10 15:15:34 -0300

    a

--- prototipo.php
+++ prototipo.php
@@ -1,3 +1,11 @@
 <?php
-echo "Esta es la primer pagina";
-123
+echo "<p><h1>Esta es la primer pagina</h1></p>";
+$nombre = "Eze";
+echo "<h1> Mi nombre es $nombre</h1>";
+$edad = 18;
+echo "<h2><p> Mi edad es $edad</h2><p>";
+if($edad>=19){
+echo "<h2 style = 'color:green'> Muy viejo </h2>";
+}else{
+    echo "<h2 style = 'color:red'> Muy Joven mijo, coma mas </h2>";
+}
